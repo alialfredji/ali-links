f98fe2fc6b8b82fd5cddf7e956bc57763394faf3
Ali Alfredji <alfredjiali@gmail.com>
2026-08-16 10:06:36 +0200

    feat: redesign profile link page

--- src/config/site.ts
+++ src/config/site.ts
@@ -3,7 +3,7 @@ import { profile } from './profile';
 export const siteConfig = {
   name: profile.name,
   shortName: profile.name,
-  title: 'Ali Alfredji — Software Engineer & Product Builder',
+  title: 'Ali Alfredji - Software Engineer & Product Builder',
   description:
     'Ali Alfredji is a software engineer, product builder & creator helping people and businesses build & operate better with AI.',
   email: profile.email,
